87dfa2aa92a03f407f38d98764338de57b333b15
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-29 15:18:57 -0300

    Repositótio My Python Packpages criado

---
+++ colors/__init__.py
@@ -0,0 +1,30 @@
+def printwhite(txt):
+    print(f'\033[0;30m{txt}\033[m')
+
+
+def printred(txt):
+    print(f'\033[0;31m{txt}\033[m')
+
+
+def printgreen(txt):
+    print(f'\033[0;32m{txt}\033[m')
+
+
+def printyellow(txt):
+    print(f'\033[0;33m{txt}\033[m')
+
+
+def printblue(txt):
+    print(f'\033[0;34m{txt}\033[m')
+
+
+def printpurple(txt):
+    print(f'\033[0;35m{txt}\033[m')
+
+
+def printcyan(txt):
+    print(f'\033[0;36m{txt}\033[m')
+
+
+def printgray(txt):
+    print(f'\033[0;37m{txt}\033[m')

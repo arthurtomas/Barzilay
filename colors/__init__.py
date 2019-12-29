f6048e282eba56a862965f6364ba4303e2750f4d
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-29 15:25:27 -0300

    test

--- colors/__init__.py
+++ colors/__init__.py
@@ -1,5 +1,5 @@
-def printwhite(txt):
-    print(f'\033[0;30m{txt}\033[m')
+def printblue(txt):
+    print(f'\033[0;34m{txt}\033[m')
 
 
 def printred(txt):
@@ -14,17 +14,17 @@ def printyellow(txt):
     print(f'\033[0;33m{txt}\033[m')
 
 
-def printblue(txt):
-    print(f'\033[0;34m{txt}\033[m')
-
-
 def printpurple(txt):
     print(f'\033[0;35m{txt}\033[m')
 
 
-def printcyan(txt):
-    print(f'\033[0;36m{txt}\033[m')
+def printwhite(txt):
+    print(f'\033[0;30m{txt}\033[m')
 
 
 def printgray(txt):
     print(f'\033[0;37m{txt}\033[m')
+
+
+def printcyan(txt):
+    print(f'\033[0;36m{txt}\033[m')
